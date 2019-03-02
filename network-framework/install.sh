f95b2b8734b53eaa7391070de0773378b841be47
Bobby Zhang <huizhez@andrew.cmu.edu>
2019-03-02 16:56:20 -0500

    modify install so no argument needed

--- network-framework/install.sh
+++ network-framework/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # The first argument should be the peer number
 # The second argument should be the organization number
-docker exec cli scripts/install.sh $1 $2
+docker exec cli scripts/install.sh 0 1
+
+docker exec cli scripts/install.sh 0 2
