11f1b057eca44bdcd68a91762a93b8e1d49e2389
Michael Coblenz <mcoblenz@cs.cmu.edu>
2019-03-06 10:25:50 -0500

    Added * to path used in cp command for Linux compatibility.

--- src/main/scala/edu/cmu/cs/obsidian/Main.scala
+++ src/main/scala/edu/cmu/cs/obsidian/Main.scala
@@ -222,7 +222,7 @@ object Main {
             val tmpGeneratedCodePath = srcDir.resolve(Paths.get("org", "hyperledger", "fabric", "example"))
             val javaTargetLocation = Paths.get(mainName, "src", "main", "java", "org", "hyperledger", "fabric", "example")
             val copyAllGeneratedClasses : String =
-                "cp -R " + tmpGeneratedCodePath.toString + File.separator + " " + javaTargetLocation.toString
+                "cp -R " + tmpGeneratedCodePath.toString + File.separator +  "* " + javaTargetLocation.toString
             println("copying: " + copyAllGeneratedClasses)
             copyAllGeneratedClasses.!
 
