7a561c168012b770c0e96e3efc4a30a725d8effd
Michael Coblenz <mcoblenz@gmail.com>
2019-08-30 14:27:34 -0400

    Added a command to compile a client program.

--- obs-vscode-extension/src/extension.ts
+++ obs-vscode-extension/src/extension.ts
@@ -7,26 +7,41 @@ let taskProvider: vscode.Disposable | undefined;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let compileCommand = vscode.commands.registerCommand('extension.compileObsidian', () => {
+            // The code you place here will be executed every time your command is executed
+                let task = getCompilationTask();
+                if (task != undefined) {
+                    vscode.tasks.executeTask(task as vscode.Task).then( 
+                        val => {vscode.window.showInformationMessage('Compilation succeeded.');},
+                        err => {vscode.window.showInformationMessage('Compilation failed.');}
+                    )
+                }
+            }       
+    );
 
-        // Use the console to output diagnostic information (console.log) and errors (console.error)
-        // This line of code will only be executed once when your extension is activated
+    context.subscriptions.push(compileCommand);
+
+    let compileClientCommand = vscode.commands.registerCommand('extension.compileObsidianClient', () => {
+        // The code you place here will be executed every time your command is executed
+            let task = getClientCompilationTask();
+            if (task != undefined) {
+                vscode.tasks.executeTask(task as vscode.Task).then( 
+                    val => {vscode.window.showInformationMessage('Compilation succeeded.');},
+                    err => {vscode.window.showInformationMessage('Compilation failed.');}
+                )
+            }
+        }
+    );       
+    context.subscriptions.push(compileClientCommand);
         
-        // The command has been defined in the package.json file
-        // Now provide the implementation of the command with registerCommand
-        // The commandId parameter must match the command field in package.json
-        let disposable = vscode.commands.registerCommand('extension.compileObsidian', () => {
-                // The code you place here will be executed every time your command is executed
-                    let task = getCompilationTask();
-                    if (task != undefined) {
-                        vscode.tasks.executeTask(task as vscode.Task).then( 
-                            val => {vscode.window.showInformationMessage('Compilation succeeded.');},
-                            err => {vscode.window.showInformationMessage('Compilation failed.');}
-                        )
-                    }
-                }       
-        );
 
-        context.subscriptions.push(disposable);
+    context.subscriptions.push(compileCommand);
 
         let commandPromise: Thenable<vscode.Task[]> | undefined = undefined;
         console.log("in activate");
@@ -73,6 +88,22 @@ export function activate(context: vscode.ExtensionContext) {
             }
             let execution = new vscode.ShellExecution(`obsidianc ${filename}`);
             return (new vscode.Task(kind, filename, 'obsidian-compile', execution, ["$obsidian"]));
+        }
+        else {
+            return undefined;
+        }
+    }
+
+    function getClientCompilationTask(): vscode.Task | undefined {
+        let activeEditor = vscode.window.activeTextEditor
+        if (activeEditor != undefined) {
+            let filename = activeEditor.document.fileName
+            let kind: ObsidianCompilationTaskDefinition = {
+                type: 'obsidian-compile-client',
+                file: filename
+            }
+            let execution = new vscode.ShellExecution(`obsidianc --build-client ${filename}`);
+            return (new vscode.Task(kind, filename, 'obsidian-compile-client', execution, ["$obsidian"]));
         }
         else {
             return undefined;
