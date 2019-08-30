d0271cf3a65b36012eebe10a7c35f549d6a14d68
Michael Coblenz <mcoblenz@gmail.com>
2019-08-29 22:28:04 -0400

    Renamed command name to something reasonable.

--- obs-vscode-extension/src/extension.ts
+++ obs-vscode-extension/src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
         // The command has been defined in the package.json file
         // Now provide the implementation of the command with registerCommand
         // The commandId parameter must match the command field in package.json
-        let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+        let disposable = vscode.commands.registerCommand('extension.compileObsidian', () => {
                 // The code you place here will be executed every time your command is executed
                     let task = getCompilationTask();
                     if (task != undefined) {
