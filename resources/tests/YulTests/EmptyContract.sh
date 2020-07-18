11d1d91c63c8e70bddff94fa17015a6b51811911
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-18 19:43:33 -0400

    debug test script

--- resources/tests/YulTests/EmptyContract.sh
+++ resources/tests/YulTests/EmptyContract.sh
@@ -4,6 +4,6 @@ cd ../../../
 
 sbt "runMain edu.cmu.cs.obsidian.Main --yul resources/tests/YulTests/EmptyContract.obs"
 
-cd IntContainer
+cd EmptyContract
 
 solc --strict-assembly EmptyContract.yul
