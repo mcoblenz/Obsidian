233a03e9e6c35c1da91ad628e090097bb6afdcf0
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-20 22:54:38 -0400

    add status checking after calling solc compiler; discard output to /dev/null instead of printing in console

--- resources/tests/YulTests/EmptyContract.sh
+++ resources/tests/YulTests/EmptyContract.sh
@@ -6,6 +6,12 @@ sbt "runMain edu.cmu.cs.obsidian.Main --yul resources/tests/YulTests/EmptyContra
 
 cd EmptyContract
 
-solc --strict-assembly EmptyContract.yul
+solc --strict-assembly EmptyContract.yul > /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "EmptyContract test failed: solc cannot compile yul code"
+  exit 1
+fi
+
 
 rm -rf EmptyContract
