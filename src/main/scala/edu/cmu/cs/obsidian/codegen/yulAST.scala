e8c71322df32ad652d1c0687a34e7840dcf72705
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-20 22:55:59 -0400

    add blank after colon in arguments

--- src/main/scala/edu/cmu/cs/obsidian/codegen/yulAST.scala
+++ src/main/scala/edu/cmu/cs/obsidian/codegen/yulAST.scala
@@ -15,16 +15,16 @@ trait YulStatement extends YulAST
 case class TypedName (name: String, ntype: String) extends YulAST
 case class Case (value: Literal, body: Block) extends YulAST
 
-case class Literal (kind:LiteralKind.LiteralKind, value: String, vtype: String) extends Expression
+case class Literal (kind: LiteralKind.LiteralKind, value: String, vtype: String) extends Expression
 case class Identifier (name: String) extends Expression
 case class FunctionCall (functionName: Identifier, arguements: Seq[Expression]) extends Expression
 
 case class Assignment (variableNames: Seq[Identifier], value: Expression) extends YulStatement
 case class VariableDeclaration (variables: Seq[TypedName]) extends YulStatement
 case class FunctionDefinition (
-                                  name:String,
-                                  parameters:Seq[TypedName],
-                                  returnVariables:Seq[TypedName],
+                                  name: String,
+                                  parameters: Seq[TypedName],
+                                  returnVariables: Seq[TypedName],
                                   body: Block) extends YulStatement
 case class If (condition: Expression, body: Block) extends YulStatement
 case class Switch (expression: Expression, cases: Seq[Case]) extends YulStatement
@@ -32,7 +32,7 @@ case class ForLoop (pre: Block, condition: Expression, post: Block, body: Block)
 case class Break () extends YulStatement
 case class Continue () extends YulStatement
 case class Leave () extends YulStatement
-case class ExpressionStatement (expression:Expression) extends YulStatement
+case class ExpressionStatement (expression: Expression) extends YulStatement
 case class Block (statements: Seq[YulStatement]) extends YulStatement
 
 
@@ -44,7 +44,7 @@ case class Block (statements: Seq[YulStatement]) extends YulStatement
     StringLiteral = '"' ([^"\r\n\\] | '\\' .)* '"'
  */
 case class YulObject (name: String, code: Code, subObjects: Seq[YulObject], data: Seq[Data]) extends YulAST
-case class Code (block:Block) extends YulAST
+case class Code (block: Block) extends YulAST
 case class Data (name: String, hex: Seq[HexLiteral], str: Seq[StringLiteral]) extends YulAST
 case class HexLiteral (content: String) extends YulAST
 case class StringLiteral (content: String) extends YulAST
