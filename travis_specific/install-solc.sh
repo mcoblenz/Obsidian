fd1f674d7ce922ee7eabc6fd8212162d1ade5dbf
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-22 15:20:11 -0400

    try to use PPAs for ubuntu to install solc

--- travis_specific/install-solc.sh
+++ travis_specific/install-solc.sh
@@ -3,11 +3,10 @@
 set -e
 # check to see if solc folder is empty
 if [ ! -d "${HOME}/solc/" ]; then
-    mkdir -p ${HOME}/solc/
-    wget https://github.com/ethereum/solidity/releases/download/v0.6.12/solidity_0.6.12.tar.gz
-    tar -zxf solidity_0.6.12.tar.gz
-    cd solidity_0.6.12
-    ./scripts/build.sh
+  sudo add-apt-repository ppa:ethereum/ethereum
+  sudo apt-get update
+  sudo apt-get install solc
+
 else
     echo "Using cached directory."
 fi
