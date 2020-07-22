5472eda388a2a401e2087245b14f8d7a9e44d301
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-22 14:23:29 -0400

    debugging install-solc scripts

--- travis_specific/install-solc.sh
+++ travis_specific/install-solc.sh
@@ -6,7 +6,7 @@ if [ ! -d "${HOME}/solc/" ]; then
     mkdir -p ${HOME}/solc/
     wget https://github.com/ethereum/solidity/releases/download/v0.6.12/solidity_0.6.12.tar.gz
     tar -zxf solidity_0.6.12.tar.gz
-    cd solidity_0.6.12.tar.gz
+    cd solidity_0.6.12
     ./scripts/build.sh
 else
     echo "Using cached directory."
