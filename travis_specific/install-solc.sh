a8bb7eabcb7f33bf2400535c01fa5cf9747e279c
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-22 15:33:04 -0400

    try to use PPAs for ubuntu to install solc

--- travis_specific/install-solc.sh
+++ travis_specific/install-solc.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-
-set -e
-# check to see if solc folder is empty
-if [ ! -d "${HOME}/solc/" ]; then
-  sudo add-apt-repository ppa:ethereum/ethereum
-  sudo apt-get update
-  sudo apt-get install solc
-
-else
-    echo "Using cached directory."
-fi
-
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install solc
