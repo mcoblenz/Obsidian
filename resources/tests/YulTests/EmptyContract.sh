0c6ac6a02f9fb297e90c5fc6781dadedcf1205b1
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-23 16:01:26 -0400

    use docker run to run solc

--- resources/tests/YulTests/EmptyContract.sh
+++ resources/tests/YulTests/EmptyContract.sh
@@ -6,10 +6,9 @@ sbt "runMain edu.cmu.cs.obsidian.Main --yul resources/tests/YulTests/EmptyContra
 
 cd EmptyContract
 
-solc --strict-assembly EmptyContract.yul > /dev/null
-#docker run ethereum/solc:stable --strict-assembly EmptyContract.yul > /dev/null
+CURR_PATH="$( pwd -P )"
 
-#docker run -v /local/path:/sources ethereum/solc:stable -o /dev/null --abi --bin /sources/Contract.sol
+docker run -v "$CURR_PATH":/sources ethereum/solc:stable --abi --bin --strict-assembly /sources/EmptyContract.yul > /dev/null
 
 if [ $? -ne 0 ]; then
   echo "EmptyContract test failed: solc cannot compile yul code"
