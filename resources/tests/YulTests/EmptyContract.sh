87d1abc010f0d1abef6c29e36cd190d31dd1da02
MirandaChen <yinglanc@andrew.cmu.edu>
2020-07-23 10:31:10 -0400

    try to install solc directly in .travis.yml using ubuntu PPA

--- resources/tests/YulTests/EmptyContract.sh
+++ resources/tests/YulTests/EmptyContract.sh
@@ -6,7 +6,10 @@ sbt "runMain edu.cmu.cs.obsidian.Main --yul resources/tests/YulTests/EmptyContra
 
 cd EmptyContract
 
-docker run ethereum/solc:stable --strict-assembly EmptyContract.yul > /dev/null
+solc --strict-assembly EmptyContract.yul > /dev/null
+#docker run ethereum/solc:stable --strict-assembly EmptyContract.yul > /dev/null
+
+#docker run -v /local/path:/sources ethereum/solc:stable -o /dev/null --abi --bin /sources/Contract.sol
 
 if [ $? -ne 0 ]; then
   echo "EmptyContract test failed: solc cannot compile yul code"
