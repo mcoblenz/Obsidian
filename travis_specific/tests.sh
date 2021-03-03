1068a1394eb75c724991674438225562ea13495e
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-03-03 10:05:05 -0500

    commenting out fabric tests

--- travis_specific/tests.sh
+++ travis_specific/tests.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 # Fabric tests
-cd resources/tests/FabricTests
+## cd resources/tests/FabricTests || exit
 
-bash IntContainerTest.sh
-bash MultipleConstructorsTest.sh
-bash TransactionInConstructorTest.sh
+## todo/iev: commenting this out to hopefully avoid some redundant work and debug just the ganache part of this
+#bash IntContainerTest.sh
+#bash MultipleConstructorsTest.sh
+#bash TransactionInConstructorTest.sh
 
 # Yul tests -- note that these only test if the compiler runs without an error
-cd ../YulTests
+cd ../YulTests || exit 1
 bash EmptyContract.sh
 
 # Ganache Tests -- these actually build the compiled Yul via Truffle then run it via Ganache
-cd ../GanacheTests
+cd ../GanacheTests || exit 1
 bash EmptyContract.sh
