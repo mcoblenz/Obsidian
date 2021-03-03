020841f4f7313506f729408bcbc4559d9b1f5c06
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-03-03 10:49:46 -0500

    debugging travis build

--- travis_specific/install_ganache.sh
+++ travis_specific/install_ganache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 npm install -g npm@7.6.0 # as of 3 march 2021 this is a recent enough npm build to support ganache and truffle
+npm install -g node # i think node ends up being out of date, too?
 npm install
 npm install -g ganache-cli truffle
--- travis_specific/tests.sh
+++ travis_specific/tests.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 # Fabric tests
-## cd resources/tests/FabricTests || exit
+## cd resources/tests/FabricTests || exit 1
 
 ## todo/iev: commenting this out to hopefully avoid some redundant work and debug just the ganache part of this
 #bash IntContainerTest.sh
 #bash MultipleConstructorsTest.sh
 #bash TransactionInConstructorTest.sh
 
+echo "--- iev --- current working directory:"
+pwd
+
 # Yul tests -- note that these only test if the compiler runs without an error
-cd ../YulTests || exit 1
+cd ../YulTests || exit 1 ## fixing this relative path to an absolute one
 bash EmptyContract.sh
 
 # Ganache Tests -- these actually build the compiled Yul via Truffle then run it via Ganache
