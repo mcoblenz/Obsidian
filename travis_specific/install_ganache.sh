3e94e46d7e2c099d22f72fc78a6dd43583276eed
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-03-04 16:13:13 -0500

    going big with version increments

--- travis_specific/install_ganache.sh
+++ travis_specific/install_ganache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm install -g npm@7.6.0 # as of 3 march 2021 this is a recent enough npm build to support ganache and truffle
+npm install -g npm # as of 3 march 2021 this is a recent enough npm build to support ganache and truffle
 npm install
 npm install -g ganache-cli truffle
