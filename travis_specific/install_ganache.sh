a166138482768cad9097500a04af709dc4eccb82
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-03-04 10:42:07 -0500

    debugging travis build

--- travis_specific/install_ganache.sh
+++ travis_specific/install_ganache.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install -y nodejs
 npm install -g npm@7.6.0 # as of 3 march 2021 this is a recent enough npm build to support ganache and truffle
 npm install
 npm install -g ganache-cli truffle
