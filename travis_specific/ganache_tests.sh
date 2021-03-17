0777a616a5b99337cea033876cce8b9722f02ff7
Michael Coblenz <mcoblenz@cs.cmu.edu>
2021-03-17 12:08:07 -0400

    Use $TRAVIS_BUILD_DIR to find the repository directory.

--- travis_specific/ganache_tests.sh
+++ travis_specific/ganache_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$OBS" || exit 1
+cd "$TRAVIS_BUILD_DIR" || exit 1
 
 # Ganache Tests -- these actually build the compiled Yul via Truffle then run it via Ganache
 for test in resources/tests/GanacheTests/*.sh
