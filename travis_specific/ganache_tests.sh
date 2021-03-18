6d47b60f624f8d48f4e88b66b691d714cfffea41
Michael Coblenz <mcoblenz@umd.edu>
2021-03-18 11:03:27 -0400

    Capture return status from individual tests and fail on any failures.

--- travis_specific/ganache_tests.sh
+++ travis_specific/ganache_tests.sh
@@ -3,8 +3,17 @@
 cd "$TRAVIS_BUILD_DIR" || exit 1
 
 # Ganache Tests -- these actually build the compiled Yul via Truffle then run it via Ganache
+
+ANY_FAILURES=0
+
 for test in resources/tests/GanacheTests/*.sh
 do
   echo "running Ganache Test $test"
-  bash "$test"
+  TEST_RESULT=$($test)
+  echo "$TEST_RESULT"
+
+  if [ $? -ne 0 ] ; then ANY_FAILURES=1 ; fi
 done
+
+exit $ANY_FAILURES
+
