93eb338811eeb1f997f8364e46aec7746b52cab2
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-03-30 14:26:20 -0400

    tidying up for a PR

--- travis_specific/tests.sh
+++ travis_specific/tests.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
 # Fabric tests
-## cd resources/tests/FabricTests || exit 1
+cd resources/tests/FabricTests || exit 1
 
-## todo/iev: commenting this out to hopefully avoid some redundant work and debug just the ganache part of this
-#bash IntContainerTest.sh
-#bash MultipleConstructorsTest.sh
-#bash TransactionInConstructorTest.sh
-
-echo "--- iev --- current working directory:"
-pwd
+bash IntContainerTest.sh
+bash MultipleConstructorsTest.sh
+bash TransactionInConstructorTest.sh
 
 # Yul tests -- note that these only test if the compiler runs without an error
 for test in resources/tests/YulTests/*.sh
@@ -17,5 +13,3 @@ do
   echo "running Yul Test $test"
   bash "$test"
 done
-
-# todo: rename this file "yul tests" or something to reflect what it actually contains
