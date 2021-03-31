6cd351a1f17b976819d8ca01b771a2ab07a87b05
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-03-31 15:34:03 -0400

    debugging path manipulations in testing script

--- travis_specific/tests.sh
+++ travis_specific/tests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Fabric tests
 cd resources/tests/FabricTests || exit 1
@@ -7,8 +7,13 @@ bash IntContainerTest.sh
 bash MultipleConstructorsTest.sh
 bash TransactionInConstructorTest.sh
 
+cd ../../../
+
 # Yul tests -- note that these only test if the compiler runs without an error
-for test in resources/tests/YulTests/*.sh
+cd resources/tests/YulTests || exit 1
+
+echo "running Yul tests, currently at $( pwd -P)"
+for test in *.sh
 do
   echo "running Yul Test $test"
   bash "$test"
