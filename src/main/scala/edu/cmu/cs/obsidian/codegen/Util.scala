764af8ecf18452536d1619f9c19342a7fe1f9bbf
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-04-15 09:53:07 -0400

    whitespace

--- src/main/scala/edu/cmu/cs/obsidian/codegen/Util.scala
+++ src/main/scala/edu/cmu/cs/obsidian/codegen/Util.scala
@@ -7,10 +7,10 @@ import org.bouncycastle.util.encoders.Hex
 object Util {
   def brace(str: String): String = s"{$str}"
   def paren(str: String): String = s"($str)"
-  def ilit(i: Int): Literal = Literal(LiteralKind.number,i.toString,"int")
-  def blit(b : Boolean): Literal = Literal(LiteralKind.boolean,b.toString,"bool")
-  def hexlit(s: String): Literal = Literal(LiteralKind.number,s,"int")
-  def callvaluecheck = codegen.If(FunctionCall(Identifier("callvalue"),Seq()),Block(Seq(ExpressionStatement(FunctionCall(Identifier("revert"),Seq(ilit(0),ilit(0)))))))
+  def ilit(i: Int): Literal = Literal(LiteralKind.number, i.toString, "int")
+  def blit(b : Boolean): Literal = Literal(LiteralKind.boolean, b.toString, "bool")
+  def hexlit(s: String): Literal = Literal(LiteralKind.number, s, "int")
+  def callvaluecheck = codegen.If(FunctionCall(Identifier("callvalue"), Seq()), Block(Seq(ExpressionStatement(FunctionCall(Identifier("revert"), Seq(ilit(0), ilit(0)))))))
 
   val true_lit: Literal = blit(true)
   val false_lit: Literal = blit(false)
@@ -23,7 +23,7 @@ object Util {
       case "int" => "u256"
       case "string" => "string"
       case "Int256" => "int256"
-      case "unit" => assert(assertion = false, "unimplemented: unit type not encoded in Yul");""
+      case "unit" => assert(assertion = false, "unimplemented: unit type not encoded in Yul"); ""
       case _ => assert(assertion = false, "yul codegen encountered an obsidian type without a mapping to the ABI"); ""
     }
   }
@@ -34,7 +34,7 @@ object Util {
 
   def keccak256(s: String): String = {
     val digestK: Keccak.Digest256 = new Keccak.Digest256()
-    s"0x${Hex.toHexString(digestK.digest(s.getBytes).slice(0,4))}" //todo: i'm not sure if it should be the first or last 4.
+    s"0x${Hex.toHexString(digestK.digest(s.getBytes).slice(0, 4))}" //todo: i'm not sure if it should be the first or last 4.
   }
 
   def hashOfFunctionDef(f: FunctionDefinition): String = {
