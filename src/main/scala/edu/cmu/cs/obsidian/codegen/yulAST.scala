89598490d76f2fa0af4d6feddc40af508c18a84f
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-04-16 09:38:15 -0400

    whitespace, scala style fixes

--- src/main/scala/edu/cmu/cs/obsidian/codegen/yulAST.scala
+++ src/main/scala/edu/cmu/cs/obsidian/codegen/yulAST.scala
@@ -1,9 +1,10 @@
 package edu.cmu.cs.obsidian.codegen
 
-import java.io.{FileReader, StringWriter}
 import com.github.mustachejava.DefaultMustacheFactory
 import edu.cmu.cs.obsidian.codegen
 import edu.cmu.cs.obsidian.codegen.Util._
+
+import java.io.{FileReader, StringWriter}
 
 
 // reminder: use abstract class if want to create a base class that requires constructor arguments
@@ -15,18 +16,21 @@ object LiteralKind extends Enumeration {
     val boolean: codegen.LiteralKind.Value = Value("bool")
     val string: codegen.LiteralKind.Value = Value("string")
 }
+
 trait Expression extends YulAST
+
 trait YulStatement extends YulAST
 
 // for each asm struct, create a case class
-case class TypedName (name: String, ntype: String) extends YulAST
-case class Case (value: Literal, body: Block) extends YulAST {
+case class TypedName(name: String, ntype: String) extends YulAST
+
+case class Case(value: Literal, body: Block) extends YulAST {
     override def toString: String = {
         s"case ${value.toString} ${brace(body.toString)}"
     }
 }
 
-case class Literal (kind: LiteralKind.LiteralKind, value: String, vtype: String) extends Expression {
+case class Literal(kind: LiteralKind.LiteralKind, value: String, vtype: String) extends Expression {
     override def toString: String = {
         /* todo/iev: i'm not positive if these assertions are the best idea, but they might be
            a nice seatbelt. the constants i'm checking against may be wrong or incomplete right now,
@@ -34,9 +38,9 @@ case class Literal (kind: LiteralKind.LiteralKind, value: String, vtype: String)
         */
         val msg: String = "internal error: literal with inconsistent type string"
         kind match {
-            case edu.cmu.cs.obsidian.codegen.LiteralKind.number => assert(vtype=="int",msg)
-            case edu.cmu.cs.obsidian.codegen.LiteralKind.boolean => assert(vtype=="bool",msg)
-            case edu.cmu.cs.obsidian.codegen.LiteralKind.string => assert(vtype=="string",msg)
+            case edu.cmu.cs.obsidian.codegen.LiteralKind.number => assert(vtype == "int", msg)
+            case edu.cmu.cs.obsidian.codegen.LiteralKind.boolean => assert(vtype == "bool", msg)
+            case edu.cmu.cs.obsidian.codegen.LiteralKind.string => assert(vtype == "string", msg)
         }
         // from the spec, "Unless it is the default type, the type of a literal has to be specified
         // after a colon", which seems like this should be what we want to do:
@@ -48,74 +52,82 @@ case class Literal (kind: LiteralKind.LiteralKind, value: String, vtype: String)
         value
     }
 }
-case class Identifier (name: String) extends Expression {
+
+case class Identifier(name: String) extends Expression {
     override def toString: String = {
         name
     }
 }
 
-case class FunctionCall (functionName: Identifier, arguments: Seq[Expression]) extends Expression {
+case class FunctionCall(functionName: Identifier, arguments: Seq[Expression]) extends Expression {
     override def toString: String = {
-        s"${functionName.toString}${paren(arguments.map(id=>id.toString).mkString(", "))}"
+        s"${functionName.toString}${paren(arguments.map(id => id.toString).mkString(", "))}"
     }
 }
 
-case class Assignment (variableNames: Seq[Identifier], value: Expression) extends YulStatement {
+case class Assignment(variableNames: Seq[Identifier], value: Expression) extends YulStatement {
     override def toString: String = {
         s"let ${variableNames.map(id => id.name).mkString(", ")} := ${value.toString}"
     }
 }
-case class VariableDeclaration (variables: Seq[TypedName]) extends YulStatement {
+
+case class VariableDeclaration(variables: Seq[TypedName]) extends YulStatement {
     override def toString: String = {
-        s"let ${variables.map(id => id.name+":"+id.ntype).mkString(", ")}"
+        s"let ${variables.map(id => id.name + ":" + id.ntype).mkString(", ")}"
     }
 }
-case class FunctionDefinition (
-                                  name: String,
-                                  parameters: Seq[TypedName],
-                                  returnVariables: Seq[TypedName],
-                                  body: Block) extends YulStatement {
+
+case class FunctionDefinition(
+                                 name: String,
+                                 parameters: Seq[TypedName],
+                                 returnVariables: Seq[TypedName],
+                                 body: Block) extends YulStatement {
     override def toString: String = {
         val mf = new DefaultMustacheFactory()
-        val mustache = mf.compile(new FileReader("Obsidian_Runtime/src/main/yul_templates/function.mustache"),"function")
+        val mustache = mf.compile(new FileReader("Obsidian_Runtime/src/main/yul_templates/function.mustache"), "function")
         val scope = new FuncScope(this)
         mustache.execute(new StringWriter(), scope).toString
     }
 }
 
-case class If (condition: Expression, body: Block) extends YulStatement{
+case class If(condition: Expression, body: Block) extends YulStatement {
     override def toString: String = {
         s"if ${condition.toString} ${brace(body.toString)}"
     }
 }
 
-case class Switch (expression: Expression, cases: Seq[Case]) extends YulStatement{
+case class Switch(expression: Expression, cases: Seq[Case]) extends YulStatement {
     override def toString: String = {
-        s"switch ${expression.toString}" + "\n" + (if(cases.isEmpty) brace("") else cases.map(c => c.toString).mkString("\n")) + "\n"
+        s"switch ${expression.toString}" + "\n" + (if (cases.isEmpty) brace("") else cases.map(c => c.toString).mkString("\n")) + "\n"
     }
 }
 
-case class ForLoop (pre: Block, condition: Expression, post: Block, body: Block) extends YulStatement {
+case class ForLoop(pre: Block, condition: Expression, post: Block, body: Block) extends YulStatement {
     override def toString: String = {
         s"for ${brace(pre.toString)} ${condition.toString} ${brace(post.toString)}" + "\n" +
-          brace(body.toString)
+            brace(body.toString)
     }
 }
-case class Break () extends YulStatement {
+
+case class Break() extends YulStatement {
     override def toString: String = "break"
 }
-case class Continue () extends YulStatement {
+
+case class Continue() extends YulStatement {
     override def toString: String = "continue"
 }
-case class Leave () extends YulStatement {
+
+case class Leave() extends YulStatement {
     override def toString: String = "leave"
 }
-case class ExpressionStatement (expression: Expression) extends YulStatement{
+
+case class ExpressionStatement(expression: Expression) extends YulStatement {
     override def toString: String = {
         expression.toString
     }
 }
-case class Block (statements: Seq[YulStatement]) extends YulStatement {
+
+case class Block(statements: Seq[YulStatement]) extends YulStatement {
     override def toString: String = {
         statements.map(s => s.toString).mkString(" ")
     }
@@ -129,20 +141,24 @@ case class Block (statements: Seq[YulStatement]) extends YulStatement {
     HexLiteral = 'hex' ('"' ([0-9a-fA-F]{2})* '"' | '\'' ([0-9a-fA-F]{2})* '\'')
     StringLiteral = '"' ([^"\r\n\\] | '\\' .)* '"'
  */
-case class Code (block: Block) extends YulAST
-case class Data (name: String, hex: Seq[HexLiteral], str: Seq[StringLiteral]) extends YulAST
-case class HexLiteral (content: String) extends YulAST
-case class StringLiteral (content: String) extends YulAST
-case class YulObject (name: String, code: Code, subObjects: Seq[YulObject], data: Seq[Data]) extends YulAST {
+case class Code(block: Block) extends YulAST
+
+case class Data(name: String, hex: Seq[HexLiteral], str: Seq[StringLiteral]) extends YulAST
+
+case class HexLiteral(content: String) extends YulAST
+
+case class StringLiteral(content: String) extends YulAST
+
+case class YulObject(name: String, code: Code, subObjects: Seq[YulObject], data: Seq[Data]) extends YulAST {
     def yulString(): String = {
         val mf = new DefaultMustacheFactory()
-        val mustache = mf.compile(new FileReader("Obsidian_Runtime/src/main/yul_templates/object.mustache"),"example")
+        val mustache = mf.compile(new FileReader("Obsidian_Runtime/src/main/yul_templates/object.mustache"), "example")
         val scope = new ObjScope(this)
         val raw: String = mustache.execute(new StringWriter(), scope).toString
         // mustache butchers certain characters; this fixes that. there may be other characters that
         // need to be added here
-        raw.replaceAll("&amp;","&").
-            replaceAll("&gt;",">").
+        raw.replaceAll("&amp;", "&").
+            replaceAll("&gt;", ">").
             replaceAll("&#10;", "\n").
             replaceAll("&#61;", "=")
     }
@@ -155,9 +171,6 @@ case class YulObject (name: String, code: Code, subObjects: Seq[YulObject], data
     //   this is to track down an example)
     // temporary function, not designed for a full recursive walk through of the object
     class ObjScope(obj: YulObject) {
-        class Func(val code: String){}
-        class Case(val hash: String){}
-        class Call(val call: String){}
 
         val mainContractName: String = obj.name
         val creationObject: String = mainContractName
@@ -168,6 +181,27 @@ case class YulObject (name: String, code: Code, subObjects: Seq[YulObject], data
         var dispatchArray: Array[codegen.Case] = Array[codegen.Case]()
         var deployCall: Array[Call] = Array[Call]()
         var memoryInitRuntime: String = ""
+
+        def dispatchEntry(f: FunctionDefinition): Seq[YulStatement] = {
+            Seq(
+                //    if callvalue() { revert(0, 0) }
+                callvaluecheck,
+                // abi_decode_tuple_(4, calldatasize())
+                codegen.ExpressionStatement(FunctionCall(Identifier("abi_decode_tuple"), Seq(ilit(4), FunctionCall(Identifier("calldatasize"), Seq())))),
+                //    fun_retrieve_24()
+                codegen.ExpressionStatement(FunctionCall(Identifier(functionRename(f.name)), f.parameters.map(p => Identifier(p.name)))), //todo: second argument is highly speculative
+                //    let memPos := allocate_memory(0)
+                codegen.Assignment(Seq(Identifier("memPos")), FunctionCall(Identifier("allocate_memory"), Seq(ilit(0)))),
+                //    let memEnd := abi_encode_tuple__to__fromStack(memPos)
+                codegen.Assignment(Seq(Identifier("memEnd")), FunctionCall(Identifier("abi_encode_tuple_to_fromStack"), Seq(Identifier("memPos")))),
+                //    return(memPos, sub(memEnd, memPos))
+                codegen.ExpressionStatement(FunctionCall(Identifier("return"), Seq(Identifier("memPos"), FunctionCall(Identifier("sub"), Seq(Identifier("memEnd"), Identifier("memPos"))))))
+            )
+        }
+
+        def deploy(): Array[Call] = deployCall
+
+        def deployFunctions(): Array[Func] = deployFunctionArray
 
         for (s <- obj.code.block.statements) {
             s match {
@@ -186,23 +220,7 @@ case class YulObject (name: String, code: Code, subObjects: Seq[YulObject], data
             }
         }
 
-        def dispatchEntry(f : FunctionDefinition) : Seq[YulStatement] =
-        {
-            Seq(
-                //    if callvalue() { revert(0, 0) }
-                callvaluecheck,
-                // abi_decode_tuple_(4, calldatasize())
-                codegen.ExpressionStatement(FunctionCall(Identifier("abi_decode_tuple"),Seq(ilit(4),FunctionCall(Identifier("calldatasize"),Seq())))),
-                //    fun_retrieve_24()
-                codegen.ExpressionStatement(FunctionCall(Identifier(functionRename(f.name)),f.parameters.map(p => Identifier(p.name)))), //todo: second argument is highly speculative
-                //    let memPos := allocate_memory(0)
-                codegen.Assignment(Seq(Identifier("memPos")),FunctionCall(Identifier("allocate_memory"),Seq(ilit(0)))),
-                //    let memEnd := abi_encode_tuple__to__fromStack(memPos)
-                codegen.Assignment(Seq(Identifier("memEnd")),FunctionCall(Identifier("abi_encode_tuple_to_fromStack"),Seq(Identifier("memPos")))),
-                //    return(memPos, sub(memEnd, memPos))
-                codegen.ExpressionStatement(FunctionCall(Identifier("return"),Seq(Identifier("memPos"),FunctionCall(Identifier("sub"),Seq(Identifier("memEnd"),Identifier("memPos"))))))
-            )
-        }
+        def runtimeFunctions(): Array[Func] = runtimeFunctionArray
 
         for (sub <- obj.subObjects) { // TODO separate runtime object out as a module (make it verbose)
             for (s <- sub.code.block.statements) { // temporary fix due to issue above
@@ -225,10 +243,15 @@ case class YulObject (name: String, code: Code, subObjects: Seq[YulObject], data
             }
         }
 
-        def deploy(): Array[Call] = deployCall
-        def deployFunctions(): Array[Func] = deployFunctionArray
-        def runtimeFunctions(): Array[Func] = runtimeFunctionArray
         def dispatchCase(): codegen.Switch = codegen.Switch(Identifier("selector"), dispatchArray.toSeq)
-        def defaultReturn(): FunctionCall = FunctionCall(Identifier("return"),Seq(ilit(0),ilit(0)))
+
+        def defaultReturn(): FunctionCall = FunctionCall(Identifier("return"), Seq(ilit(0), ilit(0)))
+
+        class Func(val code: String) {}
+
+        class Case(val hash: String) {}
+
+        class Call(val call: String) {}
     }
+
 }
