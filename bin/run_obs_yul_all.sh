78de9385654c2a5d34bdc255d8f9d9ef7260fe91
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-05-26 15:32:53 -0400

    Adding comments to convenience shell scripts

--- bin/run_obs_yul_all.sh
+++ bin/run_obs_yul_all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-# this script is kind of a hack but it works for now and saves a rewrite of
-# the run_obs_yul script
+# this is a wrapper on the run_obs_yul script, which produces yul for the ganache tests programs
+# without actually running them, that runs it on all of the test programs in a batch mode. it is
+# kind of a hack but it works for now and saves a rewrite of the run_obs_yul script.
 
 for f in $(run_obs_yul.sh | tail -n +2)
 do
