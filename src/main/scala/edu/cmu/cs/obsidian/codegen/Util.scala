86d4940e410de8b6fab84c8c5a034344502e01bb
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-08-31 17:02:24 -0400

    adding some helper functions in util. one is a renaming for transactions from child contracts; the other is basically a type-checked comment pointing out uses of some pretty substantian technical debt

--- src/main/scala/edu/cmu/cs/obsidian/codegen/Util.scala
+++ src/main/scala/edu/cmu/cs/obsidian/codegen/Util.scala
@@ -245,4 +245,25 @@ object Util {
     def sizeOfContractType(ct: edu.cmu.cs.obsidian.typecheck.ContractType): Int = {
         32
     }
+
+    /**
+      * given an expression, produce the name of the contract that it associates with.
+      * WARNING: this is a stub! actually implementing this means reworking the translation to yul
+      * to carry around type information for the expression being elaborated.
+      *
+      * @param e the expression of interest
+      * @return the name of the contract for the expression, if there is one; raises an error otherwise
+      */
+    def getContractName(e: edu.cmu.cs.obsidian.parser.Expression): String = "IntContainer"
+
+    /**
+      * given a contract name and a transaction name, produce the name of the contract in the flat
+      * yul object that corresponds to it
+      *
+      * @param contractName the name of the contract
+      * @param transactionName the name of the transaction
+      * @return the name of the Yul transaction for that contract
+      */
+    def transactionNameMapping(contractName: String, transactionName: String): String =
+        s"${contractName}___${transactionName}"
 }
