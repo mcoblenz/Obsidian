dfa42865023aad008af9c717541a57d10cc53f74
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-10-26 16:08:47 -0400

    copying over a helper from type checker tests to test on actual contracts in files

--- src/test/scala/edu/cmu/cs/obsidian/codegen/UtilTest.scala
+++ src/test/scala/edu/cmu/cs/obsidian/codegen/UtilTest.scala
@@ -11,7 +11,28 @@ import scala.collection.mutable.ArrayBuffer
 
 class UtilTest extends JUnitSuite {
 
+    private def runTest(file: String, expectedSize: Int): Unit = {
+        var prog: Program = null
+        try {
+            prog = Parser.parseFileAtPath(file, new FileInputStream(file), printTokens = false)
+        }
+        catch {
+            case p: Parser.ParseException =>
+                val errMsg = p.message
+                fail(s"Failed with parser message $errMsg")
+        }
 
+        val (importsProcessedAst, _) = ImportProcessor.processImports(file, List(), prog)
+        val fieldsLiftedAst = StateFieldTransformer.transformProgram(importsProcessedAst)
+
+        val table = new SymbolTable(fieldsLiftedAst)
+        val (globalTable: SymbolTable, transformErrors) = StateNameValidator.transformProgram(table)
+
+        val checker = new Checker(globalTable)
+        val errs = (checker.checkProgram()._1 ++ transformErrors).sorted
+
+        assertTrue(s"The size of the contracts in ${file} did not match the expected sizes", true)
+    }
 
     @Test def sizeOfOneInt() : Unit = {
         val simple : ContractType = new ContractType("Simple", Seq(IntType()))
