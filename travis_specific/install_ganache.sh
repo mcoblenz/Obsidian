993d65ee2fe74e25889c14e06062626024f3927c
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-11-18 16:59:33 -0500

    monkeying around with the install script

--- travis_specific/install_ganache.sh
+++ travis_specific/install_ganache.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install libcryptx-perl # perl crypto library that implements Keccak256
-sudo apt install nodejs
+#sudo apt install nodejs
 
 npm install -g npm
 npm install -g ganache-cli
