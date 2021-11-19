13fb65ccdc3e528cb09234bc2172c7b82b0f0138
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-11-19 12:35:02 -0500

    moving npm commands around

--- travis_specific/install_ganache.sh
+++ travis_specific/install_ganache.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install libcryptx-perl # perl crypto library that implements Keccak256
 
-npm install -g npm
-npm install -g ganache-cli
+# npm install -g npm
+# npm install -g ganache-cli
 
 sudo snap install jq # a commandline JSON tool
