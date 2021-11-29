b81988059ee503f79fa37a0ec7720fa5acd7b1d7
Ian Voysey <ivoysey@users.noreply.github.com>
2021-11-29 15:08:21 -0500

    support integer arguments to the main function (#400)
    * adding a test case to show that an old bug got fixed. closes #334
    
    * adding a new test case to show that arguments are not currently passed to main
    
    * adding a different presentation of the tests info while i'm thinking of it
    
    * fixing a bad change to IDE settings
    
    * adding a yul statement for comments
    
    * adding helpers to compute abi decoder functions and insert them into the dispatch table and emit them in the body of the object
    
    * updating test cases
    
    * removing old test
    
    * removing old test
    
    * code to decode individual ints, using that to decode tuples, emitting it in the output file
    
    * i forget to change the name of the contract every single time i copy a test
    
    * traveseing the params not the returns
    
    * renaming, scraps
    
    * fixing an error in the ABI names we map to
    
    * removing a todo
    "
    
    * scraps, scala style
    
    * adding parameters to the json for the two tests that use them; the technical debt here is quickly becoming too much but i want to make sure this works before doing a pass at rewriting the test harness
    
    * removing duplicate decoders from output, only assigning params to locals if there are more than 0 of them. this should make the old tests pass again
    
    * using distinct instead of hard to read translations to and from set
    
    * Adding some documentation and removing some todos
    
    * fixing json for mainargs1, updating test script to read json for the two new tests in a slightly hacky and specific way
    
    * increasing the padding size on arguments
    
    * test.json is kind of part of a rewrite of the test infrastructure and not really part of this PR. so it doesn't belong in this branch anyway AND is breaking the tests
    
    * removing some dead boilerplate
    
    * updating the padding to the right length in the test script; removing a dead call to decode
    
    * scala style and whitespace
    
    * scraps, comments

--- src/main/scala/edu/cmu/cs/obsidian/codegen/CodeGenYul.scala
+++ src/main/scala/edu/cmu/cs/obsidian/codegen/CodeGenYul.scala
@@ -393,9 +393,6 @@ object CodeGenYul extends CodeGenerator {
 
         // the result is the recursive translation and the expression either using the temp
         // here or not.
-
-        // todo: this does not work with non-void functions that are called without binding
-        //  their results, ie "f()" if f returns an int
         ids.map(id => decl_0exp(id)) ++
             seqs.flatten ++ (width match {
             case 0 => Seq(ExpressionStatement(FunctionCall(Identifier(name), thisID +: ids)))
@@ -513,17 +510,17 @@ object CodeGenYul extends CodeGenerator {
                 // arguments and invoke the constructor as normal transaction with the hash appended
                 // to the name to call the right one
                 val conCall =
-                    if (checkedTable.contract(contractType.contractName).get.contract.declarations.exists(d => isMatchingConstructor(d))) {
-                        translateInvocation(name = transactionNameMapping(contractType.contractName, contractType.contractName) + hashOfFunctionName(contractType.contractName, typeNames),
-                            args = args,
-                            obstype = Some(UnitType()),
-                            thisID = id_memaddr,
-                            retvar = retvar, contractName = contractName, checkedTable = checkedTable, inMain = inMain)
-                    } else {
-                        Seq()
-                    }
+                if (checkedTable.contract(contractType.contractName).get.contract.declarations.exists(d => isMatchingConstructor(d))) {
+                    translateInvocation(name = transactionNameMapping(contractType.contractName, contractType.contractName) + hashOfFunctionName(contractType.contractName, typeNames),
+                        args = args,
+                        obstype = Some(UnitType()),
+                        thisID = id_memaddr,
+                        retvar = retvar, contractName = contractName, checkedTable = checkedTable, inMain = inMain)
+                } else {
+                    Seq()
+                }
 
-                Seq(// grab the appropriate amount of space of memory sequentially, off the free memory pointer
+                Seq( // grab the appropriate amount of space of memory sequentially, off the free memory pointer
                     decl_1exp(id_memaddr, apply("allocate_memory", intlit(sizeOfContractST(contractType.contractName, checkedTable)))),
 
                     // return the address that the space starts at
