402137c9681986e92039f18e12a9fcac8e8dadcd
Jason Yih <jjyih2@gmail.com>
2021-12-07 18:45:28 -0500

    changed the .json, .sol, and .obs files for AssignLocalAdd, test_gas.sh added

--- resources/tests/SolidityTests/AssignLocalAdd.sol
+++ resources/tests/SolidityTests/AssignLocalAdd.sol
@@ -3,15 +3,8 @@
 pragma solidity >=0.4.18 <0.9.0;
 
 contract AssignLocalAdd {
-    function AssignLAdd() public pure {
-        int x;
-        bool y;
-        x = 5 + 12;
-        return;
-    }
-    
-    function main() public pure returns (int) {
-        int x = 5 + 12;
+    function main(int y) public pure returns (int) {
+        int x = 5 + y;
         return x;
     }
 }
