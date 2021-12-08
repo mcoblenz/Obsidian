bab69083d7d729b720202b55b8dded9dfde0bca2
Ian Ernest Voysey <iev@cs.cmu.edu>
2021-12-08 16:42:08 -0500

    test script rewrite progress

--- travis_specific/ganache_tests.py
+++ travis_specific/ganache_tests.py
@@ -26,9 +26,18 @@ def check_for_command(cmd):
         error(f"{cmd} not installed")
 
 
+# return { result -> pass/fail, progress -> list of strings, reason -> string, non empty if we're in fail }
 def run_one_test(test_info, verbose):
     ganache_host = "http://localhost:8545"
+    obsidian_jar = "" # todo
+    progress = []
     # compile the obsidian file in question to yul with a jar of obsidianc
+    comp_result = subprocess.run(
+        ["java", "-jar", obsidian_jar, "--yul", f"resources/tests/GanacheTests/{test_info['file']}.obs"], capture_output=True)
+    if not comp_result.returncode == 0:
+        return {'result': "fail", 'progress': progress, "reason": f"obsidianc run failed with output {comp_result.stderr}"}
+    else:
+        progress = progress + ["compiled obsidian to yul"]
 
     # compile the yul to evm with solc
 
@@ -50,10 +59,8 @@ def run_one_test(test_info, verbose):
 
     # kill ganache
 
-    if len(test_info['file']) > 6:
-        return None
-    else:
-        return "mock fail"
+    return {'result': "pass", 'progress': progress, "reason": ""}
+
 
 
 # todo; grab this off the commandline, also verbosity
@@ -74,8 +81,8 @@ tests_data = json.load(f)
 f.close()
 
 # compare the files present to the tests described, producing a warning in either direction
-files_with_tests = sorted([test['file'] for test in tests_data['tests']])
-files_present = sorted(os.path.basename(obs) for obs in glob.glob(test_dir + '*.obs'))
+files_with_tests = [test['file'] for test in tests_data['tests']]
+files_present = [os.path.basename(obs) for obs in glob.glob(test_dir + '*.obs')]
 
 extra_files = list(set(files_present) - set(files_with_tests))
 extra_test_descriptions = list(set(files_with_tests) - set(files_present))
@@ -88,15 +95,30 @@ if extra_test_descriptions:
     warn("there are described tests that do not have present obsidian files:\n\t" + pprint.pformat(
         extra_test_descriptions))
 
+verbose = True
+
 # todo: make the obsidianc jar
+if verbose:
+    print("running sbt build")
+
+build = subprocess.run(["sbt" "\'set assembly / test := {}\'", ++"$TRAVIS_SCALA_VERSION", "assembly"], capture_output=True)
 
 # todo: add ability to do one offs not just the whole suite
 failed = []
 for test in tests_data['tests']:
     result = run_one_test(test, False)
-    if not result:
+    if result['result'] == "pass":
         print(colored("PASS:", 'green'), test['file'])
-    else:
+    elif result['result'] == "fail":
         print(colored("FAIL:", 'red'), test['file'])
         print(f"\t{result}")
         failed = failed + [test['file']]
+    else:
+        error(f"test script error: result from test was neither pass nor fail, got {result['result']}")
+
+if len(failed) == 0:
+    print(colored(f"\nALL {str(len(tests_data['tests']))} TESTS PASSED", 'green'))
+    sys.exit(0)
+else:
+    print(colored(f"\n{len(failed)}/{str(len(tests_data['tests']))} TESTS FAILED", 'red'))
+    sys.exit(1)
